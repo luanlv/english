5ec6260cca8bb7bce6b53ee41a94d8018597d7f2
luan <mrvluan@gmail.com>
2016-02-05 06:55:39 +0700

    fix

--- public/javascripts/app2.js
+++ public/javascripts/app2.js
@@ -10,6 +10,84 @@ api.rd = function(name){
   //console.log("Debug: " + name)
 };
 
+var Cookie =
+{
+  set: function(name, value, days)
+  {
+    var domain, domainParts, date, expires, host;
+
+    if (days)
+    {
+      date = new Date();
+      date.setTime(date.getTime()+(days*24*60*60*1000));
+      expires = "; expires="+date.toGMTString();
+    }
+    else
+    {
+      expires = "";
+    }
+
+    host = location.host;
+    if (host.split('.').length === 1)
+    {
+      // no "." in a domain - it's localhost or something similar
+      document.cookie = name+"="+value+expires+"; path=/";
+    }
+    else
+    {
+      // Remember the cookie on all subdomains.
+      //
+      // Start with trying to set cookie to the top domain.
+      // (example: if user is on foo.com, try to set
+      //  cookie to domain ".com")
+      //
+      // If the cookie will not be set, it means ".com"
+      // is a top level domain and we need to
+      // set the cookie to ".foo.com"
+      domainParts = host.split('.');
+      domainParts.shift();
+      domain = '.'+domainParts.join('.');
+
+      document.cookie = name+"="+value+expires+"; path=/; domain="+domain;
+
+      // check if cookie was successfuly set to the given domain
+      // (otherwise it was a Top-Level Domain)
+      if (Cookie.get(name) == null || Cookie.get(name) != value)
+      {
+        // append "." to current domain
+        domain = '.'+host;
+        document.cookie = name+"="+value+expires+"; path=/; domain="+domain;
+      }
+    }
+  },
+
+  get: function(name)
+  {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i=0; i < ca.length; i++)
+    {
+      var c = ca[i];
+      while (c.charAt(0)==' ')
+      {
+        c = c.substring(1,c.length);
+      }
+
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+  },
+
+  erase: function(name)
+  {
+    Cookie.set(name, '', -1);
+  }
+};
+api.Cookie = Cookie;
+
+var cookie = Cookie.get('venglish');
+Cookie.erase('venglish');
+Cookie.set('venglish', cookie);
 
 
 api.post = function(post){
@@ -1676,7 +1754,11 @@ var UserButton = function(ctrl){ return (
               "Profile"
             ]}, 
             {tag: "br", attrs: {}}, 
-            {tag: "a", attrs: {href:"/logout", class:"fluid ui small button"}, children: [
+            {tag: "a", attrs: {href:"/logout", class:"fluid ui small button", 
+              onclick:function(){
+                api.Cookie.erase('venglish');
+              }
+            }, children: [
               {tag: "i", attrs: {class:"sign out icon"}}, 
               "Logout"
             ]}
@@ -3040,85 +3122,7 @@ if(document.domain === "localhost"){
   wsCtrl.static = ""
 }
 
-var Cookie =
-{
-  set: function(name, value, days)
-  {
-    var domain, domainParts, date, expires, host;
 
-    if (days)
-    {
-      date = new Date();
-      date.setTime(date.getTime()+(days*24*60*60*1000));
-      expires = "; expires="+date.toGMTString();
-    }
-    else
-    {
-      expires = "";
-    }
-
-    host = location.host;
-    if (host.split('.').length === 1)
-    {
-      // no "." in a domain - it's localhost or something similar
-      document.cookie = name+"="+value+expires+"; path=/";
-    }
-    else
-    {
-      // Remember the cookie on all subdomains.
-      //
-      // Start with trying to set cookie to the top domain.
-      // (example: if user is on foo.com, try to set
-      //  cookie to domain ".com")
-      //
-      // If the cookie will not be set, it means ".com"
-      // is a top level domain and we need to
-      // set the cookie to ".foo.com"
-      domainParts = host.split('.');
-      domainParts.shift();
-      domain = '.'+domainParts.join('.');
-
-      document.cookie = name+"="+value+expires+"; path=/; domain="+domain;
-
-      // check if cookie was successfuly set to the given domain
-      // (otherwise it was a Top-Level Domain)
-      if (Cookie.get(name) == null || Cookie.get(name) != value)
-      {
-        // append "." to current domain
-        domain = '.'+host;
-        document.cookie = name+"="+value+expires+"; path=/; domain="+domain;
-      }
-    }
-  },
-
-  get: function(name)
-  {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i=0; i < ca.length; i++)
-    {
-      var c = ca[i];
-      while (c.charAt(0)==' ')
-      {
-        c = c.substring(1,c.length);
-      }
-
-      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
-  },
-
-  erase: function(name)
-  {
-    Cookie.set(name, '', -1);
-  }
-};
-
-
-var cookie = Cookie.get('venglish');
-console.log(cookie)
-Cookie.erase('venglish');
-Cookie.set('venglish', cookie);
 
 
 wsCtrl.ping = 0;
